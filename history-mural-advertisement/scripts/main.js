9da27624c59c23a1f07166479d2453c03dda1791
youkoso-hhe <bagsvoicedevelopz@gmail.com>
2025-09-06 23:28:02 +0900

    Update main.js

--- history-mural-advertisement/scripts/main.js
+++ history-mural-advertisement/scripts/main.js
@@ -82,7 +82,7 @@ function showNextPiece() {
   adElement.style.clipPath = `url(#${pieceData.clipPathId})`;
 
   const adClient = 'ca-pub-4222268682478872'; // ID
-  const adSlot = '7927145069'; // 自分のスロットID
+  const adSlot = '7758218160'; // 自分のスロットID
 
   adElement.innerHTML = `<ins class="adsbygoogle" style="display:block; width:100%; height:100%;" data-ad-client="${adClient}" data-ad-slot="${adSlot}"></ins>`;
 
@@ -132,6 +132,7 @@ window.addEventListener('load', () => {
   loadPage(currentPageIndex);
   translateUI();
 });
+
 
 
 
